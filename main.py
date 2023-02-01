ea51a0d10ccfe120330907d7b1984346cde757fa
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2023-02-01 14:16:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title('This is a title')
-st.title('aaaaaaaaaaaaaa')
+st.title('estudando estudando estudando')
 st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
