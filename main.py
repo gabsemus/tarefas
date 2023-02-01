a6675724cbd273342f433a83c80655fd1b7b08e9
LuizaBissoni <luiza.bissoni@hotmail.com>
2023-02-01 19:56:53 -0300

    atualizando

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 import streamlit as st
 
-st.title('Teste')
-st.title('estudando estudando estudando')
-st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
+st.title('Lista de Tarefas')
