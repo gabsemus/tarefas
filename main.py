7fda2101fcc73bea83050d72abd3b3079355fe8b
LuizaBissoni <luiza.bissoni@hotmail.com>
2023-02-01 21:11:15 -0300

    atualizando

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.title('Lista de Tarefas')
+#st.title('Lista de Tarefas')
+
+st.sidebar.title('Menu')
+pagSelecionada = st.sidebar.selectbox('Selecione uma opção',['Inserir Tarefa', 'Todas as Tarefas'])
+
+if pagSelecionada == 'Inserir Tarefa':
+    st.title('Inserir Tarefa')
+
+else:
+    pagSelecionada == 'Todas as Tarefas'
+    st.title('Tarefas')
