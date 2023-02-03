d4bd23ee76daae91041a07bef61b9e89ae8afaa0
LuizaBissoni <luiza.bissoni@hotmail.com>
2023-02-03 18:11:00 -0300

    commit

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import streamlit as st
 
 st.title('Tarefas da House')
 
-def menu():
-    st.sidebar.title('Menu')
-    pagSelecionada = st.sidebar.selectbox('Selecione uma opção',['Inserir Tarefa', 'Todas as Tarefas'])
+st.sidebar.title('Menu')
+pagSelecionada = st.sidebar.selectbox('Selecione uma opção',['Inserir Tarefa', 'Todas as Tarefas'])
 
-    if pagSelecionada == 'Inserir Tarefa':
-        st.header('Inserir Tarefa')
+if pagSelecionada == 'Inserir Tarefa':
+    st.header('Inserir Tarefa')
 
-        with st.form(key='icluirTarefa'):
-            tarefa = st.text_input(label='Tarefa: ')
-            data = st.date_input('Data')
-            hora = st.time_input('Hora')
-            botao = st.form_submit_button('Inserir')
+    with st.form(key='icluirTarefa'):
+        tarefa = st.text_input(label='Tarefa: ')
+        data = st.date_input('Data')
+        hora = st.time_input('Hora')
+        botao = st.form_submit_button('Adicionar')
     
     
 
 
-    elif pagSelecionada == 'Todas as Tarefas':
-        st.header('Tarefas')
+elif pagSelecionada == 'Todas as Tarefas':
+    st.header('Tarefas')
